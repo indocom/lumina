338842476da7be1584b90314fd26e7d11623e7f9
Simon Julian Lauw <51783522+simonjulianl@users.noreply.github.com>
2021-08-06 22:32:25 +0800

    update other credentials (#27)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  title: 'NUS Guide',
-  tagline: 'Bring me to NUS',
-  url: 'https://benclmnt.github.io',
-  baseUrl: '/lumina/',
-  favicon: 'img/logo.png',
-  organizationName: 'benclmnt', // Usually your GitHub org/user name.
-  projectName: 'lumina', // Usually your repo name.
+  title: "NUS Guide",
+  tagline: "Bring me to NUS",
+  url: "https://guide.pinusonline.org",
+  baseUrl: "/lumina/",
+  favicon: "img/logo.png",
+  organizationName: "PINUS Tech", // Usually your GitHub org/user name.
+  projectName: "lumina", // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'NUSGuide',
+      title: "NUSGuide",
       logo: {
-        alt: 'NUSGuide Logo',
-        src: 'img/logo.png',
+        alt: "NUSGuide Logo",
+        src: "img/logo.png",
       },
       hideOnScroll: true,
       items: [
@@ -22,26 +22,26 @@ module.exports = {
         //   position: 'left',
         // },
         {
-          to: 'docs/after/introduction',
-          activeBasePath: 'docs/after',
-          label: 'After Offer',
-          position: 'left',
+          to: "docs/after/introduction",
+          activeBasePath: "docs/after",
+          label: "After Offer",
+          position: "left",
         },
         {
-          to: 'docs/cca/cca-index',
-          activeBasePath: 'docs/cca',
-          label: 'CCAs and Clubs',
-          position: 'left',
+          to: "docs/cca/cca-index",
+          activeBasePath: "docs/cca",
+          label: "CCAs and Clubs",
+          position: "left",
         },
         {
-          to: 'docs/contributors',
-          label: 'Contributors',
-          position: 'right',
+          to: "docs/contributors",
+          label: "Contributors",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'light',
+      style: "light",
       links: [
         // {
         //   title: 'Before Acceptance',
@@ -53,80 +53,83 @@ module.exports = {
         //   ],
         // },
         {
-          title: 'After Acceptance',
+          title: "After Acceptance",
           items: [
             {
-              label: 'Introduction',
-              to: 'docs/after/introduction',
+              label: "Introduction",
+              to: "docs/after/introduction",
             },
             {
-              label: 'Dormitory',
-              to: 'docs/after/housing'
+              label: "Dormitory",
+              to: "docs/after/housing",
             },
             {
-              label: 'Financial Aid',
-              to: 'docs/after/financial-aid',
+              label: "Financial Aid",
+              to: "docs/after/financial-aid",
             },
           ],
         },
         {
-          title: 'Social',
+          title: "Social",
           items: [
             {
-              label: 'Facebook',
-              to: 'https://www.facebook.com/PerhimpunanIndonesiaNUS/',
+              label: "Facebook",
+              to: "https://www.facebook.com/PerhimpunanIndonesiaNUS/",
             },
             {
-              label: 'Instagram',
-              href: 'https://www.instagram.com/pinusonline/',
+              label: "Instagram",
+              href: "https://www.instagram.com/pinusonline/",
             },
             {
-              label: 'Github',
-              href: 'https://www.github.com/benclmnt/lumina',
+              label: "Github",
+              href: "https://www.github.com/indocom/lumina",
             },
           ],
         },
       ],
-      copyright: `This mirror is maintained by <a href="https://github.com/benclmnt" target="_blank" rel="noopener noreferrer">Bennett Clement</a>.`,
+      copyright: `This mirror is maintained by <a href="https://github.com/indocom" target="_blank" rel="noopener noreferrer">PINUS Tech</a>.`,
     },
     announcementBar: {
       id: "outdated",
-      content: 'These docs might contain some outdated information from 2020. If you are an NUS freshman, please fill in <a target="_blank" rel="noopener noreferrer" href="https://tinyurl.com/PINUSFreshman2021">this survey</a>',
+      content:
+        'These docs might contain some outdated information from 2020. If you are an NUS freshman, please fill in <a target="_blank" rel="noopener noreferrer" href="https://tinyurl.com/PINUSFreshman2021">this survey</a>',
       isCloseable: false,
       backgroundColor: "#ffba00",
     },
     gtag: {
-      trackingID: 'G-PDF8GH7GWE',
+      trackingID: "G-PDF8GH7GWE",
       // Optional fields.
       anonymizeIP: false, // Should IPs be anonymized?
     },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/benclmnt/lumina/edit/master',
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/indocom/lumina/edit/master",
           showLastUpdateTime: true,
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
         sitemap: {
-          changefreq: 'weekly',
+          changefreq: "weekly",
           priority: 0.5,
           trailingSlash: false,
-        }
+        },
       },
     ],
   ],
   plugins: [
-    '@docusaurus/plugin-ideal-image',
-    [require.resolve('@cmfcmf/docusaurus-search-local'), {
-      indexBlog: false,
-      docsRouteBasePath: '/docs'
-    }]
+    "@docusaurus/plugin-ideal-image",
+    [
+      require.resolve("@cmfcmf/docusaurus-search-local"),
+      {
+        indexBlog: false,
+        docsRouteBasePath: "/docs",
+      },
+    ],
   ],
 };
